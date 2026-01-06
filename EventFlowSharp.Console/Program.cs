8e9ff0743f8e944148c1d27c4c88eec868220671
ArchLeaders <archleadership28@gmail.com>
2026-01-06 01:19:18 -0800

    Update console runner to print Flowchart and EntryPoint names

--- EventFlowSharp.Console/Program.cs
+++ EventFlowSharp.Console/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EventFlowSharp;
+using EventFlowSharp.EVFL;
+using EventFlowSharp.ORE;
 
-Console.WriteLine("Hello, World!");
+unsafe {
+    byte[] buffer = File.ReadAllBytes(@"D:\bin\.todo\EventFlowSharp\TestData\DmF_SY_SleepHotel.110.bfevfl");
+    ref ResEventFlowFile evfl = ref CafeEventFlowFile.GetRes(buffer);
+    Console.WriteLine(evfl.Header.GetFileName().ToString());
+
+    evfl.Relocate();
+
+    for (int i = 0; i < evfl.FlowchartCount; i++) {
+        ref ResFlowchart flowchart = ref evfl.Flowcharts.GetPtr()[i].Get();
+        Console.WriteLine(new StringView(flowchart.Name.Get().Data).ToString());
+
+        ResEntryPoint* entryPoints = flowchart.EntryPoints.GetPtr();
+
+        ref ResDic dic = ref flowchart.EntryPointNames.Get();
+        ResDicEntry* entries = dic.GetEntries();
+        for (int entryIndex = 1; entryIndex < dic.EntryCount; entryIndex++) {
+            ref var entry = ref entries[entryIndex];
+            Console.WriteLine(entry.GetKey().ToString());
+
+            ref var entryPoint = ref entryPoints[i];
+            Console.WriteLine(entryPoint.SubFlowEventIndicesCount);
+            Console.WriteLine(entryPoint.VariableDefsCount);
+        }
+    }
+}
