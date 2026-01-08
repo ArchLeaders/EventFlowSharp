688e606f74178011ab60be74037f69b2628bba03
ArchLeaders <archleadership28@gmail.com>
2026-01-07 17:57:15 -0800

    Fix packing in ResEvent unions

--- EventFlowSharp.EVFL/ResEvent.cs
+++ EventFlowSharp.EVFL/ResEvent.cs
@@ -4,7 +4,7 @@ using EventFlowSharp.ORE;
 namespace EventFlowSharp.EVFL;
 
 [Swappable]
-[StructLayout(LayoutKind.Explicit)]
+[StructLayout(LayoutKind.Explicit, Size = 40, Pack = 2)]
 public partial struct ResEvent
 {
     [FieldOffset(0)]
@@ -39,17 +39,17 @@ public partial struct ResEvent
 }
 
 [Swappable]
-[StructLayout(LayoutKind.Sequential, Size = 30)]
+[StructLayout(LayoutKind.Sequential, Size = 30, Pack = 2)]
 public partial struct ResActionEvent
 {
-    public ushort NextEventIndex;
+    public short NextEventIndex;
     public ushort ActorIndex;
     public ushort ActorActionIndex;
     public BinaryPointer<ResMetaData> Params;
 }
 
 [Swappable]
-[StructLayout(LayoutKind.Sequential, Size = 30)]
+[StructLayout(LayoutKind.Sequential, Size = 30, Pack = 2)]
 public partial struct ResSwitchEvent
 {
     public ushort CaseCount;
@@ -60,7 +60,7 @@ public partial struct ResSwitchEvent
 }
 
 [Swappable]
-[StructLayout(LayoutKind.Explicit, Size = 30)]
+[StructLayout(LayoutKind.Explicit, Size = 30, Pack = 2)]
 public partial struct ResForkEvent
 {
     [FieldOffset(0)]
@@ -74,18 +74,18 @@ public partial struct ResForkEvent
 }
 
 [Swappable]
-[StructLayout(LayoutKind.Sequential, Size = 30)]
+[StructLayout(LayoutKind.Sequential, Size = 30, Pack = 2)]
 public partial struct ResJoinEvent
 {
-    public ushort NextEventIndex;
+    public short NextEventIndex;
 }
 
 [Swappable]
-[StructLayout(LayoutKind.Explicit, Size = 30)]
+[StructLayout(LayoutKind.Explicit, Size = 30, Pack = 2)]
 public partial struct ResSubFlowEvent
 {
     [FieldOffset(0)]
-    public ushort NextEventIndex;
+    public short NextEventIndex;
     
     [FieldOffset(6)]
     public BinaryPointer<ResMetaData> Params;
