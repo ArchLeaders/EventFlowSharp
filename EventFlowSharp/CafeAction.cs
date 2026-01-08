454633bb66a68e84ea719f476b32ae76d64d230c
ArchLeaders <archleadership28@gmail.com>
2026-01-07 17:57:57 -0800

    Update Action/Query ToString method and debug display

--- EventFlowSharp/CafeAction.cs
+++ EventFlowSharp/CafeAction.cs
@@ -1,8 +1,13 @@
+using System.Diagnostics;
+
 namespace EventFlowSharp;
 
+[DebuggerDisplay("{Action}")]
 public class CafeAction
 {
     public required string Action { get; set; }
     
     public required CafeActor Actor { get; set; }
+
+    public override string ToString() => Action;
 }
--- EventFlowSharp/CafeQuery.cs
+++ EventFlowSharp/CafeQuery.cs
@@ -1,8 +1,13 @@
+using System.Diagnostics;
+
 namespace EventFlowSharp;
 
+[DebuggerDisplay("{Query}")]
 public class CafeQuery
 {
     public required string Query { get; set; }
     
     public required CafeActor Actor { get; set; }
+    
+    public override string ToString() => Query;
 }
